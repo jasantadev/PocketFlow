29d9a9cacdcd5f1ff35d9587e6485c9776ebfaa6
jasantadev <jasantadev@gmail.com>
2026-03-03 22:39:48 +0100

    Registro y configuracion de modelos en el admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from .models import Category, Movement
 
 # Register your models here.
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
-    list_display = ["title", "monthly_budget"]
+    list_display = ["user", "title", "monthly_budget"]
     list_display_links = ["user"]
     list_filter = ["user"]
-    search_fields = ["user", "title"]
+    search_fields = ["user__username", "title"]
 
-@admin.register(Category, CategoryAdmin)
+@admin.register(Movement)
+class MovementAdmin(admin.ModelAdmin):
+    model = Movement
+    list_display = ["user", "amount", "category", "date", "type"]
+    list_display_links = ["user", "category"]
+    list_filter = ["category", "user", "date", "type"]
+    search_fields = ["user__username", "date"]
