bbdadb22bc9a23d78340c48f610fc0dd7bbe01e2
jasantadev <jasantadev@gmail.com>
2026-03-06 06:17:07 +0100

    Configuramos función get_balance para calcular el capital total por usuario en el UserSerializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,14 +1,22 @@
 from .models import Category, Movement
 from rest_framework import serializers
-from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db.models import Sum, Value
+from django.db.models.functions import Coalesce
 
 
 
 class UserSerializer(serializers.ModelSerializer):
-    #Tengo que introducir campo calculado -> Capital Total, sumando y restando todos los movimiento del Usuario
+    balance = serializers.SerializerMethodField()
     class Meta:
-        model = settings.AUTH_USER_MODEL
-        fields = ('id', 'username', 'email')
+        model = get_user_model()
+        fields = ('id', 'username', 'email', 'balance')
+
+    def get_balance(self, obj):
+        total_income = obj.movements.filter(type=Movement.MovementType.INCOME).aggregate(total=Coalesce(Sum("amount"), Value(0)))
+        total_expense = obj.movements.filter(type=Movement.MovementType.EXPENSE).aggregate(total=Coalesce(Sum("amount"), Value(0)))
+        return(total_income["total"] - total_expense["total"])
+        
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
